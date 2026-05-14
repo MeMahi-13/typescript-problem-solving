3908b3a569abfb9752ea1d23e8f7ad8d0027d235
Mohima Chowdhury <work.mohima@gmail.com>
2026-05-14 11:13:45 +0600

    added problem solving

---
+++ solutions.ts
@@ -0,0 +1,60 @@
+function filterEvenNumbers(arr: number[]): number[] {
+  return arr.filter(num => num % 2 === 0);
+}
+
+
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+
+function checkType(value: string | number): string {
+  if (typeof value === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+}
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+  
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter(num => arr2.includes(num));
+}
